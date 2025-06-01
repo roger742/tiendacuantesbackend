6b0d5c13d46b8879f38e4fbf54d5f1bc6c6ce9dd
roger742 <chimalsanz24@gmail.com>
2025-05-31 21:50:30 -0600

    Update product.entity.ts
    Se agrego los cambios a las tablas de la BD

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,19 +1,38 @@
-import { Entity, Column, ManyToOne, DeleteDateColumn } from 'typeorm';
+import { Entity, Column, ManyToOne, DeleteDateColumn, PrimaryGeneratedColumn, JoinColumn } from 'typeorm';
 import { Category } from '../../categories/entities/category.entity';
 
 @Entity('products')
 export class Product {
-  @Column({ primary: true, generated: true})
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column({ type: 'varchar', length: 255, nullable: false })
   name: string;
 
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  image: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
   @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
   price: number;
 
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
+  unitPrice: number;
+
+  @Column({ type: 'int', nullable: false })
+  stock: number;
+
+  @Column({ name: 'categoryId' })
+  categoryId: number;
+
   @ManyToOne(() => Category, (category) => category.products, { onDelete: 'CASCADE' })
-  category: Category;  
+  @JoinColumn({ name: 'categoryId' })
+  category: Category;
+
+  @Column({ default: true })
+  isActive: boolean;
 
   @DeleteDateColumn()
   deletedAt: Date;
